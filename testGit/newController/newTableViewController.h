7557094df2a67d1a1ab27065395d080ac69368ae
payliu <pay@ms6.url.com.tw>
2011-06-26 17:32:37 +0800

    add newTabelViewController

---
+++ testGit/newController/newTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  newTableViewController.h
+//  testGit
+//
+//  Created by Pay on 2011/6/26.
+//  Copyright 2011年 (Pay). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface newTableViewController : UITableViewController {
+    
+}
+
+@end
